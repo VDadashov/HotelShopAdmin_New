7daeafc47334894bcfcef21a4755d539a514b4b9
Vusal Dadashov <vusald79@gmail.com>
2025-09-27 11:27:21 +0400

    update env

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -157,7 +157,7 @@ export const Login = () => {
             </Formik>
             <div className="mt-6 text-center">
               <p className="text-xs text-gray-500 dark:text-gray-400">
-                DevIT Group Admin Panel
+                Rockvell Group Admin Panel
               </p>
             </div>
           </CardContent>
